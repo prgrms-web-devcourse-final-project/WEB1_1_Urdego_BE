2b5d84941ec0fe2fed5b1310e6be637b3745839c
ooni <gdbsrjsdn@gmail.com>
2024-11-22 11:12:20 +0900

    Feature(#6) : 그룹 관련 Port 구조 작성

---
+++ group_service/src/main/java/io/urdego/group_service/application/port/in/GroupMemberService.java
@@ -0,0 +1,4 @@
+package io.urdego.group_service.application.port.in;
+
+public interface GroupMemberService {
+}
---
+++ group_service/src/main/java/io/urdego/group_service/application/service/GroupMemberServiceImpl.java
@@ -0,0 +1,4 @@
+package io.urdego.group_service.application.service;
+
+public class GroupMemberServiceImpl {
+}
