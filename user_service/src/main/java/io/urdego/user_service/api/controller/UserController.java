fb264209a24b89ad4869f9a1063968996fc1ead1
Anyeon00 <seluo65@naver.com>
2024-11-24 02:18:53 +0900

    Feature(#14): Swagger 추가

--- user_service/src/main/java/io/urdego/user_service/api/controller/UserController.java
+++ user_service/src/main/java/io/urdego/user_service/api/controller/UserController.java
@@ -1,5 +1,7 @@
 package io.urdego.user_service.api.controller;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.urdego.user_service.api.controller.request.SignInRequest;
 import io.urdego.user_service.api.controller.request.SignUpRequest;
 import io.urdego.user_service.api.controller.request.VerifyNicknameRequest;
@@ -19,22 +21,23 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/users")
-    public ResponseEntity<Long> postUser(@RequestBody final SignUpRequest request){
+    @ApiResponse(responseCode = "200", description = "응답 예시 : Hongildong12")
+    public ResponseEntity<Long> postUser(@RequestBody final SignUpRequest request) {
         Long userId = userService.signUp(request);
         return ResponseEntity.ok(userId);
     }
 
     @PostMapping("/nickname")
+    @ApiResponse(responseCode = "200", description = "응답 예시 : PERMIT / DUPLICATED")
     public ResponseEntity<String> postNickname(@RequestBody final VerifyNicknameRequest request) {
         NicknameVerificationResult result = userService.verifyNickname(request.nickname());
         return ResponseEntity.ok(result.getStatus());
     }
 
     @PostMapping("/login")
+    @ApiResponse(responseCode = "200", description = "응답 예시 : Hongildong12")
     public ResponseEntity<String> signIn(@RequestBody final SignInRequest request) {
         String nickname = userService.login(request.email(), request.password());
         return ResponseEntity.ok(nickname);
     }
-
-
 }
