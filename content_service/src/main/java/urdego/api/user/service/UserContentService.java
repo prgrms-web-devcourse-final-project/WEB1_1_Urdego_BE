87e47400c0912ce43ef8c4def7183655f80b798d
JoungWoo Lee <j_ra1n@naver.com>
2024-11-25 20:05:56 +0900

    Feature: 단일 컨텐츠 업로드 구현
    * S3 저장 + 메타데이터 저장 + 클라이언트 입력 저장

--- content_service/src/main/java/urdego/api/user/service/UserContentService.java
+++ content_service/src/main/java/urdego/api/user/service/UserContentService.java
@@ -1,3 +1,10 @@
 package urdego.api.user.service;
 
-public class UserContentService {}
+import org.springframework.web.multipart.MultipartFile;
+import urdego.api.user.controller.request.ContentUploadRequest;
+
+public interface UserContentService {
+
+    // 단일 컨텐츠 등록
+    void uploadContent(ContentUploadRequest request, MultipartFile file);
+}
