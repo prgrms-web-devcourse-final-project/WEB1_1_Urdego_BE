304b4fcd028eb311990d71ff03fdeb0bd659349f
ooni <gdbsrjsdn@gmail.com>
2024-11-26 16:00:03 +0900

    Feature(#30) : content-service에 swagger 추가

---
+++ content_service/src/main/java/io/urdego/content_service/common/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package io.urdego.content_service.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Content Service API")
+                                .description("Documentation for Content Service API")
+                                .version("v1.0"));
+    }
+}
+
