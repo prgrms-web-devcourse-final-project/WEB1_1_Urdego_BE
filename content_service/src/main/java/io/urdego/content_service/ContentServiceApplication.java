31417a7e97c38e8b15ac30d88fd6734320320977
Ban-gilhyeon <bbgiloo98@gmail.com>
2024-11-26 20:42:45 +0900

    Refactor : 배포 테스트

--- content_service/src/main/java/io/urdego/content_service/ContentServiceApplication.java
+++ content_service/src/main/java/io/urdego/content_service/ContentServiceApplication.java
@@ -2,6 +2,7 @@ package io.urdego.content_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
