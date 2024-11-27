7b6a2c0b1efa60b55fdbbffd33b42ba275243bff
ooni <gdbsrjsdn@gmail.com>
2024-11-27 17:08:07 +0900

    Feature(#56) : 예외 처리

---
+++ game_service/src/main/java/io/urdego/game_service/common/exception/problem/ProblemException.java
@@ -0,0 +1,9 @@
+package io.urdego.game_service.common.exception.problem;
+
+import io.urdego.game_service.common.exception.BaseException;
+
+public class ProblemException extends BaseException {
+    public ProblemException(String message) {
+        super(message);
+    }
+}
