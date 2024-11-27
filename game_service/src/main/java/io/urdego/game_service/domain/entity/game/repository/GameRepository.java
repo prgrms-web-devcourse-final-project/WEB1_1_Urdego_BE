7b16f200e06099200992a56f5d4ad3241a27d43c
ooni <gdbsrjsdn@gmail.com>
2024-11-27 17:14:44 +0900

    Feature(#56) : repostory 빈 등록

--- game_service/src/main/java/io/urdego/game_service/domain/entity/game/repository/GameRepository.java
+++ game_service/src/main/java/io/urdego/game_service/domain/entity/game/repository/GameRepository.java
@@ -2,10 +2,11 @@ package io.urdego.game_service.domain.entity.game.repository;
 
 import io.urdego.game_service.domain.entity.game.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-
+@Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
     Optional<Game> findByGroupId(Long groupId);
 }
--- game_service/src/main/java/io/urdego/game_service/domain/entity/problem/repository/ProblemRepository.java
+++ game_service/src/main/java/io/urdego/game_service/domain/entity/problem/repository/ProblemRepository.java
@@ -2,9 +2,11 @@ package io.urdego.game_service.domain.entity.problem.repository;
 
 import io.urdego.game_service.domain.entity.problem.Problem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProblemRepository extends JpaRepository<Problem, Long> {
     List<Problem> findByRoundId(Long roundId);
 }
