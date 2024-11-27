95246ec99c8bf393a6e7d34b498c6646e43b733d
ooni <gdbsrjsdn@gmail.com>
2024-11-27 17:14:44 +0900

    Feature(#56) : repostory 빈 등록

--- game_service/src/main/java/io/urdego/game_service/domain/entity/player/repository/PlayerRepository.java
+++ game_service/src/main/java/io/urdego/game_service/domain/entity/player/repository/PlayerRepository.java
@@ -2,9 +2,11 @@ package io.urdego.game_service.domain.entity.player.repository;
 
 import io.urdego.game_service.domain.entity.player.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
     List<Player> findByGameId(Long gameId);
 }
