c75ee03d474183043665dc9b8af60bb41305ab41
JoungWoo Lee <j_ra1n@naver.com>
2024-11-27 18:07:58 +0900

    Feature: 컨텐츠 조회

--- content_service/src/main/java/io/urdego/content_service/api/user/controller/UserContentController.java
+++ content_service/src/main/java/io/urdego/content_service/api/user/controller/UserContentController.java
@@ -1,12 +1,15 @@
 package io.urdego.content_service.api.user.controller;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.urdego.content_service.api.user.controller.request.ContentUploadRequest;
+import io.urdego.content_service.api.user.controller.response.UserContentListAndCursorIdxResponse;
 import io.urdego.content_service.api.user.service.UserContentService;
 import io.urdego.content_service.common.client.UserServiceClient;
 import io.urdego.content_service.common.client.response.UserResponse;
-
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +27,7 @@ public class UserContentController {
     /*
      TODO: security 도입후 수정
     */
+    @ApiResponse(responseCode = "200", description = "컨텐츠 업로드 성공")
     @PostMapping(value = "/contents", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Void> uploadContent(
             @RequestParam("file") MultipartFile file,
@@ -50,6 +54,7 @@ public class UserContentController {
     }
 
     // 여러 컨텐츠 등록
+    @ApiResponse(responseCode = "200", description = "다중 컨텐츠 업로드 성공")
     @PostMapping(value = "/contents/multiple", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Void> uploadMultipleContents(
             @RequestParam("files") MultipartFile[] files,
@@ -77,6 +82,7 @@ public class UserContentController {
     }
 
     // 컨텐츠 단일 삭제
+    @ApiResponse(responseCode = "200", description = "컨텐츠 삭제 성공")
     @DeleteMapping(value = "{userId}/contents/{contentId}")
     public ResponseEntity<Void> deleteContent(
             @PathVariable(name = "userId") Long userId,
@@ -88,4 +94,21 @@ public class UserContentController {
         userContentService.deleteContent(contentId);
         return ResponseEntity.ok().build();
     }
+
+    // 컨텐츠 조회
+    @ApiResponse(responseCode = "200", description = "유저 컨텐츠 조회 성공", content = @Content(schema = @Schema(implementation = UserContentListAndCursorIdxResponse.class)))
+    @GetMapping(value = "{userId}/contents")
+    public ResponseEntity<UserContentListAndCursorIdxResponse> getUserContents(@PathVariable(name = "userId") Long userId,
+                                                                               @Min(value = 0) @RequestParam(name = "cursorIdx", required = false) Long cursorIdx,
+                                                                               @Min(value = 1) @RequestParam(name = "limit", defaultValue = "5") Long limit) {
+
+        // Feign 유저 검증
+        UserResponse userResponse = userServiceClient.getUserById(userId);
+
+        UserContentListAndCursorIdxResponse responses = userContentService.getUserContents(userResponse.getUserId(), cursorIdx, limit);
+
+        return ResponseEntity.ok().body(responses);
+    }
+
+
 }
