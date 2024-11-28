ecc7194bca2d4a6c00cd49b50e111421d611ed9c
ooni <gdbsrjsdn@gmail.com>
2024-11-28 23:55:59 +0900

    Feature(#56) : 빌더 패턴 추가

--- game_service/src/main/java/io/urdego/game_service/domain/entity/round/Round.java
+++ game_service/src/main/java/io/urdego/game_service/domain/entity/round/Round.java
@@ -2,6 +2,7 @@ package io.urdego.game_service.domain.entity.round;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -19,9 +20,19 @@ public class Round {
     private Long roundId;
 
     @Column(nullable = false)
-    private int roundNumber;
+    private int roundNum;
 
     // FK
     @Column(nullable = false)
     private Long gameId;
+
+    @Column(nullable = false)
+    private Long contentId;
+
+    @Builder
+    public Round(int roundNum, Long gameId, Long contentId) {
+        this.roundNum = roundNum;
+        this.gameId = gameId;
+        this.contentId = contentId;
+    }
 }
