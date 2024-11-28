4e8877f62cead8cda2a09162f335f5888ab59f86
Ban-gilhyeon <bbgiloo98@gmail.com>
2024-11-28 12:39:58 +0900

    Feature(#51) 클라이언트 SSE 연결

--- notification_service/src/main/java/com/example/notification_service/api/controller/NotificationSseController.java
+++ notification_service/src/main/java/com/example/notification_service/api/controller/NotificationSseController.java
@@ -1,34 +1,58 @@
 package com.example.notification_service.api.controller;
 
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-@RestController("/api/notification-service/sse")
+@RestController
 @RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api/notification-service/sse")
 public class NotificationSseController {
-	private final StringRedisTemplate redisTemplate;
+	private final RedisMessageListenerContainer container;
+	private final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
+	private final RedisMessageListenerContainer redisMessageListenerContainer;
 
-	@GetMapping
-	public void streamNotifications(HttpServletResponse response) throws Exception {
-		response.setContentType("text/event-stream");
-		response.setCharacterEncoding("UTF-8");
+	//클라이언트 SSE 연결
+	@GetMapping("/{userId}")
+	public SseEmitter sseEmitter(@PathVariable Long userId) {
+		//SSE 연결
+		SseEmitter emitter = new SseEmitter(Long.MAX_VALUE); // 연결 시간 최대
+		emitters.put(userId, emitter);
 
-		PrintWriter writer = response.getWriter();
+		//연결 종료
+		emitter.onCompletion(() -> emitters.remove(userId));
+		emitter.onTimeout(() -> emitters.remove(userId));
 
-		redisTemplate.getConnectionFactory().getConnection().subscribe(new MessageListener() {
+		//Redis 채널 구독 설정
+		redisMessageListenerContainer.addMessageListener(new MessageListener() {
 			@Override
 			public void onMessage(Message message, byte[] pattern) {
-				String notification = message.toString();
-				writer.write("data:" + notification + "\n\n");
-				writer.flush();
+				String channel = new String(pattern);
+				String messageStr = message.toString();
+
+				//SSE로 클라에 메세지 전송
+				if(channel.equals("user:"+userId)) {
+					try{
+						emitter.send(SseEmitter.event().data(messageStr));
+					}catch (IOException e){
+						log.error("failed to send SSE to user {}: {}", userId, e.getMessage());
+						emitters.remove(userId);
+					}
+				}
 			}
-		}, "user: *".getBytes());
+		}, new PatternTopic("user:"+userId));
+		return emitter;
 	}
 }
