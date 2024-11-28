f32ff8e774c4bd682e6efeade6053f3d21a36dd4
ooni <gdbsrjsdn@gmail.com>
2024-11-28 23:55:26 +0900

    Feature(#56) : Problem Round로 통합

--- game_service/src/main/java/io/urdego/game_service/common/exception/problem/ProblemException.java
+++
@@ -1,9 +0,0 @@
-package io.urdego.game_service.common.exception.problem;
-
-import io.urdego.game_service.common.exception.BaseException;
-
-public class ProblemException extends BaseException {
-    public ProblemException(String message) {
-        super(message);
-    }
-}
