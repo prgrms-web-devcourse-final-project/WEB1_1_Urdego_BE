426f938c8dfb3915f5d6c5e3f6ce4b4169d33053
Ban-gilhyeon <bbgiloo98@gmail.com>
2024-11-29 12:16:51 +0900

    Refactor(#59) : getGroupInfo 추가

--- group_service/src/main/java/io/urdego/group_service/api/controller/groupMember/dto/request/AddGroupMemberReq.java
+++ group_service/src/main/java/io/urdego/group_service/api/controller/groupMember/dto/request/AddGroupMemberReq.java
@@ -2,4 +2,5 @@ package io.urdego.group_service.api.controller.groupMember.dto.request;
 
 import io.urdego.group_service.domain.entity.groupMember.GroupMemberRole;
 
-public record AddGroupMemberReq(Long userId, String email, GroupMemberRole role) {}
+public record AddGroupMemberReq(Long userId,
+                                GroupMemberRole role) {}
