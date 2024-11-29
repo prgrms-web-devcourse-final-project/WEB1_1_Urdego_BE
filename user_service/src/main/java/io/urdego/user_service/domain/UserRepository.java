42298f3750937d82d0fdd0981de4576bdce916a8
JoungWoo Lee <j_ra1n@naver.com>
2024-11-29 20:33:48 +0900

    Feature: Feign userIds 검증

--- user_service/src/main/java/io/urdego/user_service/domain/UserRepository.java
+++ user_service/src/main/java/io/urdego/user_service/domain/UserRepository.java
@@ -1,9 +1,9 @@
 package io.urdego.user_service.domain;
 
 import io.urdego.user_service.domain.entity.User;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,4 +18,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("select u from User u where u.nickname like %:string%")
     List<User> findByString(String string);
+
+    // Feign userId 검증
+    @Query("SELECT u.id FROM User u WHERE u.id IN :userIds")
+    List<Long> findByUserIds(@Param("userIds") List<Long> userIds);
 }
