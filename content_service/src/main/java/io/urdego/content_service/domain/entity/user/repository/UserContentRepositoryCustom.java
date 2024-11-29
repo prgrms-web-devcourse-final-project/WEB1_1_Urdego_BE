0c6fca17b29b3fed7ebc92539f708df4faf56858
JoungWoo Lee <j_ra1n@naver.com>
2024-11-29 20:23:35 +0900

    Feature: 게임 컨텐츠 조회 구현

--- content_service/src/main/java/io/urdego/content_service/domain/entity/user/repository/UserContentRepositoryCustom.java
+++ content_service/src/main/java/io/urdego/content_service/domain/entity/user/repository/UserContentRepositoryCustom.java
@@ -7,6 +7,8 @@ import java.util.List;
 public interface UserContentRepositoryCustom {
 
     // userId를 통해 해당 유저의 컨텐츠를 조회한다.
-    List<UserContentResponse> findUserContentsByUserId_CursorPaging(
-            Long userId, Long cursorIdx, Long limit);
+    List<UserContentResponse> findUserContentsByUserId_CursorPaging(Long userId, Long cursorIdx, Long limit);
+
+    // userIds를 통해 게임 컨텐츠를 랜덤 조회한다.
+    List<UserContentResponse> findRandomContentsByUserIds(List<Long> userIds);
 }
