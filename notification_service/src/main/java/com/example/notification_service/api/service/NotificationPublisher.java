587c42a8b34244f3b0b7bf3ebc4c250a6f399427
Ban-gilhyeon <bbgiloo98@gmail.com>
2024-12-01 06:42:09 +0900

    Refactor(#51) : NotificationRequestInfo 수정

--- notification_service/src/main/java/com/example/notification_service/api/service/NotificationPublisher.java
+++ notification_service/src/main/java/com/example/notification_service/api/service/NotificationPublisher.java
@@ -1,5 +1,6 @@
 package com.example.notification_service.api.service;
 
+import com.example.notification_service.api.controller.dto.NotificationRequestInfo;
 import com.example.notification_service.common.client.GroupServiceClient;
 import com.example.notification_service.common.client.UserServiceClient;
 import com.example.notification_service.domain.NotificationMessage;
@@ -19,30 +20,28 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class NotificationPublisher {
 	private final StringRedisTemplate redisTemplate;
+	private final ObjectMapper objectMapper;
 	private final UserServiceClient userServiceClient;
 	private final GroupServiceClient groupServiceClient;
-	private final ObjectMapper objectMapper;
 
-	public void publishToUser(List<Long> targetIds, Long senderId, Long groupId){
-		//Id들 nickName 및 groupTitle로 전환
+	public void publishToUser(NotificationRequestInfo requestInfo){
+		//TODO feign 호출이 너무 많아서 배치 조회 알아보기
+		/*//Id들 nickName 및 groupName로 전환
 		String senderNickName = userServiceClient.getUser(senderId).nickname();
 		log.info("senderNickName : {}",senderNickName);
 		String groupName = groupServiceClient.getGroupInfo(groupId).groupName();
 		log.info("groupName : {}",groupName);
-		List<String> targetNickNames = new ArrayList<>();
-		for(int i = 0; i< targetIds.size(); i++){
-			targetNickNames.add(userServiceClient.getUser(targetIds.get(i)).nickname());
-			log.info("targetNickNames : {}",targetNickNames.get(i));
-
+		List<String> targetNickNames = new ArrayList<>();*/
+		for(int i = 0; i< requestInfo.targetIds().size(); i++){
 			//알림 메세지 생성
 			NotificationMessage notification = NotificationMessage.of(
 					UUID.randomUUID().toString(),
-					groupId,
-					groupName,
-					senderId,
-					senderNickName,
-					targetIds.get(i),
-					targetNickNames.get(i),
+					requestInfo.groupId(),
+					requestInfo.groupName(),
+					requestInfo.senderId(),
+					requestInfo.senderNickname(),
+					requestInfo.targetIds().get(i),
+					requestInfo.targetNicknames().get(i),
 					"게임에 초대했습니다.",
 					LocalDateTime.now()
 			);
@@ -51,17 +50,17 @@ public class NotificationPublisher {
 			try {
 				message = objectMapper.writeValueAsString(notification);
 			}catch (JsonProcessingException e){
-				log.error("failed to serialize Notification for userId: {}",targetIds.get(i));
+				log.error("failed to serialize Notification for userId: {}",requestInfo.targetIds().get(i));
 				continue;
 			}
 
 			//Pub/Sub 채널
-			String channel = "user:" + targetIds.get(i);
+			String channel = "user:" + requestInfo.targetIds().get(i);
 
 			//Redis 메세지 발행
 			redisTemplate.convertAndSend(channel, message);
 
-			log.info("from : " + senderId + "to : " + targetIds.get(i) + "message : " + message);
+			log.info("from : " + requestInfo.senderId() + "to : " + requestInfo.targetIds().get(i) + "message : " + message);
 		}
 	}
 }
--- notification_service/src/main/java/com/example/notification_service/api/service/SseService.java
+++ notification_service/src/main/java/com/example/notification_service/api/service/SseService.java
@@ -3,7 +3,7 @@ package com.example.notification_service.api.service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 public interface SseService {
-
+	void init();
 	SseEmitter connect(Long userId);
 	void disconnect(Long userId);
 }
