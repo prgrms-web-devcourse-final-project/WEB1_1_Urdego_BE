ef8f62cdbae95f77ba6ff6d45161e1518769d066
JoungWoo Lee <j_ra1n@naver.com>
2024-12-02 21:15:47 +0900

    Swagger: 설정

--- gateway_service/src/main/java/io/urdego/gateway_service/config/SwaggerConfig.java
+++ gateway_service/src/main/java/io/urdego/gateway_service/config/SwaggerConfig.java
@@ -1,49 +1,29 @@
 package io.urdego.gateway_service.config;
 
-import org.springdoc.core.models.GroupedOpenApi;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@RequiredArgsConstructor
 @Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "advisor API",
+                version = "v1",
+                description = "urdego API 입니다."
+        )
+)
 public class SwaggerConfig {
 
     @Bean
-    public GroupedOpenApi userServiceApi() {
-        return GroupedOpenApi.builder()
-                .group("user-service")
-                .pathsToMatch("/api/user-service/**")
-                .build();
+    public OpenAPI openAPI() {
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"));
     }
 
-    @Bean
-    public GroupedOpenApi contentServiceApi() {
-        return GroupedOpenApi.builder()
-                .group("content-service")
-                .pathsToMatch("/api/content-service/**")
-                .build();
-    }
-
-    @Bean
-    public GroupedOpenApi groupServiceApi() {
-        return GroupedOpenApi.builder()
-                .group("group-service")
-                .pathsToMatch("/api/group-service/**")
-                .build();
-    }
-
-    @Bean
-    public GroupedOpenApi gameServiceApi() {
-        return GroupedOpenApi.builder()
-                .group("game-service")
-                .pathsToMatch("/api/game-service/**")
-                .build();
-    }
-
-    @Bean
-    public GroupedOpenApi notificationServiceApi() {
-        return GroupedOpenApi.builder()
-                .group("notification-service")
-                .pathsToMatch("/api/notification-service/**")
-                .build();
-    }
 }
