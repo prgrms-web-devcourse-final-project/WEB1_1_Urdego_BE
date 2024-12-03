bba2ab987eca0695b48774bc5794d5d7909fad91
Anyeon00 <seluo65@naver.com>
2024-12-03 17:16:22 +0900

    Feature(#119): 그룹생성 요청DTO 수정

--- group_service/src/main/java/io/urdego/group_service/api/controller/group/api/dto/request/CreateGroupReq.java
+++ group_service/src/main/java/io/urdego/group_service/api/controller/group/api/dto/request/CreateGroupReq.java
@@ -3,13 +3,12 @@ package io.urdego.group_service.api.controller.group.api.dto.request;
 import java.util.List;
 
 public record CreateGroupReq(
-        Long userId,    //그룹 생성자 id
-        String groupName,
-        String description,
-        Integer memberLimit,
-        // Integer timer,
-        Integer totalRounds, // 라운드 수
-        List<String> invitedUserNicknames
+        String title,   //groupName
+        Integer maxPlayers, //memberLimit
+        Integer rounds,   //totalRounds
+        Integer timer,
+        String host,    //방장 닉네임    _userId 대신
+        List<String> invitedFriends
 ) {
 }
 
