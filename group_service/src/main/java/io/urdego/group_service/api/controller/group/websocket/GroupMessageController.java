ba47ed93dbbdd5c7aa70f5aa7ebee2d1ab0cdaab
Anyeon00 <seluo65@naver.com>
2024-12-04 17:50:07 +0900

    Refactor(#134): 웹 소켓 게임시작 메시지 응답 수정

--- group_service/src/main/java/io/urdego/group_service/api/controller/group/websocket/GroupMessageController.java
+++ group_service/src/main/java/io/urdego/group_service/api/controller/group/websocket/GroupMessageController.java
@@ -1,11 +1,18 @@
 package io.urdego.group_service.api.controller.group.websocket;
 
+import io.urdego.group_service.api.controller.group.api.dto.response.GroupInfoRes;
 import io.urdego.group_service.api.controller.group.websocket.request.MessageRequest;
 import io.urdego.group_service.api.controller.group.websocket.response.GameStartResponse;
 import io.urdego.group_service.api.controller.group.websocket.response.GroupStatusResponse;
 import io.urdego.group_service.api.controller.group.websocket.response.GroupMemberStatusResponse;
 import io.urdego.group_service.api.controller.group.websocket.response.MessageResponse;
+import io.urdego.group_service.api.controller.groupMember.dto.response.GroupMemberListRes;
+import io.urdego.group_service.api.controller.groupMember.dto.response.GroupMemberRes;
+import io.urdego.group_service.api.service.group.GroupService;
 import io.urdego.group_service.api.service.groupMember.GroupMemberService;
+import io.urdego.group_service.common.client.UserServiceClient;
+import io.urdego.group_service.common.client.response.ResponseUserInfo;
+import io.urdego.group_service.domain.entity.groupMember.GroupMember;
 import io.urdego.group_service.domain.entity.groupMember.GroupMemberRole;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,6 +31,8 @@ public class GroupMessageController {
 
     private final GroupMemberService groupMemberService;
     private final MessageDataReader messageDataReader;
+    private final GroupService groupService;
+    private final UserServiceClient userServiceClient;
 
     @MessageMapping("/group/{groupId}")
     @SendTo("/group-service/subscribe/group/{groupId}")
@@ -46,7 +55,15 @@ public class GroupMessageController {
             }
             case START -> {
                 Long gameId = messageDataReader.readLong(request.data().get("gameId"));
-                yield new MessageResponse<>(GameStartResponse.of(gameId));
+
+                Integer rounds = groupService.getGroupInfo(groupId).totalRounds();
+                List<String> nicknameList =
+                        groupMemberService.getMemberList(groupId).
+                                members().stream().parallel().
+                                map(GroupMemberRes::userId).
+                                map(userServiceClient::getUserById).
+                                map(ResponseUserInfo::nickname).toList();
+                yield new MessageResponse<>(GameStartResponse.of(gameId, nicknameList, rounds));
             }
         };
     }
